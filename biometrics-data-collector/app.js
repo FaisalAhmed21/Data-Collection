8d02cb985a7207b8aef952b106bd9095fb88b094
Faisal Ahmed <faisalrajshahi2003@gmail.com>
2025-07-04 16:25:55 +0600

    Update app.js

--- biometrics-data-collector/app.js
+++ biometrics-data-collector/app.js
@@ -102,9 +102,6 @@ class BiometricDataCollector {
     init() {
         this.bindEvents();
         this.generateParticipantId();
-        this.switchScreen('typing');
-        this.startTypingTask();
-
         this.initializeGallery();
         this.setupPointerTracking();
     }
@@ -169,6 +166,11 @@ class BiometricDataCollector {
 
     
     bindEvents() {
+        // Welcome screen
+        document.getElementById('start-btn').addEventListener('click', () => {
+            this.switchScreen('typing');
+            this.startTypingTask();
+        });
         
         // Typing task - FIXED: Proper mobile-friendly event handling
         const typingInput = document.getElementById('typing-input');
