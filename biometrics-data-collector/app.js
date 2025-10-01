c229d909bba5e4cc391a891a3d78cdb8bee3da55
Faisal Ahmed <faisalrajshahi2003@gmail.com>
2025-10-02 03:57:04 +0600

    Update app.js

--- biometrics-data-collector/app.js
+++ biometrics-data-collector/app.js
@@ -3622,7 +3622,6 @@ class BiometricDataCollector {
                 }
                 features.push({
                     participant_id: this.participantId,
-                    task_id: 1,
                     sentence_number: keystroke.sentence + 1, // Convert 0-based to 1-based numbering
                     timestamp_ms: Math.round(keystroke.timestamp),
                     ref_char: refChar,
